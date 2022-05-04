dfb758b727ed1ee99741f5efc457ac86a0bf9c71
Kursat Dogan <kursatdogan27900@gmail.com>
2022-05-04 10:27:08 +0300

    new step added to apply.feature file

--- src/test/java/com/userlane/UI/step_definitions/applyStepDefs.java
+++ src/test/java/com/userlane/UI/step_definitions/applyStepDefs.java
@@ -38,7 +38,7 @@ public class applyStepDefs {
 
 
     @When("the user clicks to open position for the Automation Test Engineer")
-    public void the_user_clicks_to_open_position_for_the_Automation_Test_Engineer() throws InterruptedException {
+    public void the_user_clicks_to_open_position_for_the_Automation_Test_Engineer(){
         JavascriptExecutor jse = (JavascriptExecutor) Driver.get();
         jse.executeScript("window.scrollBy(0,4000)");
 //        js.executeScript("arguments[0].scrollIntoView();", careerPage.applicationBtn);
@@ -51,9 +51,11 @@ public class applyStepDefs {
         }
 
         descriptionPage.appyPositionBtn.click();
-        for(String subWindow : Driver.get().getWindowHandles()){
-            Driver.get().switchTo().window(subWindow);
-        }
+
+    }
+
+    @When("the user enters the necessary information on the info page")
+    public void the_user_enters_the_necessary_information_on_the_info_page() throws InterruptedException {
 
         infoPage.resumeBtn.sendKeys("C:\\Users\\vdw-Kursat\\Desktop\\Kursat Dogan - Resume.pdf");
         infoPage.inputName.sendKeys(faker.name().fullName());
@@ -64,6 +66,7 @@ public class applyStepDefs {
         infoPage.inputUrl.get(1).sendKeys(faker.internet().url());
         infoPage.inputUrl.get(2).sendKeys(faker.internet().url());
         infoPage.inputUrl.get(3).sendKeys(faker.internet().url());
+        infoPage.inputUrl.get(4).sendKeys(faker.internet().url());
         infoPage.inputAvabilityAndSlary.get(0).sendKeys("4 weeks");
         infoPage.inputAvabilityAndSlary.get(1).sendKeys("**** Euro/Year Gross");
         WebDriverWait wait = new WebDriverWait(Driver.get(), 10);
@@ -73,8 +76,8 @@ public class applyStepDefs {
         infoPage.cypressRadioBtn.click();
         Thread.sleep(30000);
         infoPage.submitBtn.click();
-
     }
+
     @Then("the user should able to navigate application page")
     public void the_user_should_able_to_navigate_application_page() {
         String actualMsg = verifyPage.applicationMsg.getText();
--- src/test/resources/features/apply.feature
+++ src/test/resources/features/apply.feature
@@ -4,4 +4,5 @@ Feature: the user should able to apply open QA position
   Scenario: the user can add any item to card
     Given the user is on the career page
     When the user clicks to open position for the Automation Test Engineer
+    And the user enters the necessary information on the info page
     Then the user should able to navigate application page
