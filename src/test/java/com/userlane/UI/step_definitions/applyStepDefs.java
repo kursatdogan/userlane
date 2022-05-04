593909603cabe0c5efe2d339bdf6e2723f353063
Kursat Dogan <kursatdogan27900@gmail.com>
2022-05-04 10:47:12 +0300

    Added some explanations

--- src/test/java/com/userlane/UI/step_definitions/applyStepDefs.java
+++ src/test/java/com/userlane/UI/step_definitions/applyStepDefs.java
@@ -11,45 +11,43 @@ import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import com.github.javafaker.Faker;
 import org.junit.Assert;
-import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.sql.SQLOutput;
-
 public class applyStepDefs {
-
+    // I have created the object from pages to reach the webelements and some other functionalities of them
     CareerPage careerPage = new CareerPage();
     DescriptionPage descriptionPage = new DescriptionPage();
     InfoPage infoPage = new InfoPage();
     VerifyPage verifyPage = new VerifyPage();
+
+    // faker object invoked from Faker class to create fake informations such as name, phone number or address
     Faker faker = new Faker();
 
     @Given("the user is on the career page")
     public void the_user_is_on_the_career_page() {
 
+    // Navigating to Userlane career page
         Driver.get().get(ConfigurationReader.get("url"));
 
     }
 
-
     @When("the user clicks to open position for the Automation Test Engineer")
     public void the_user_clicks_to_open_position_for_the_Automation_Test_Engineer(){
+
+    // Scroll down with JavascriptExecutor to see the Webelement
         JavascriptExecutor jse = (JavascriptExecutor) Driver.get();
         jse.executeScript("window.scrollBy(0,4000)");
-//        js.executeScript("arguments[0].scrollIntoView();", careerPage.applicationBtn);
-//        Thread.sleep(5000);
-       // careerPage.positionBtn.click();
+
+    // Click the web element and navigate to new opening window by WindowHandle
         careerPage.applicationBtn.click();
         String parentWindow = Driver.get().getWindowHandle();
         for(String subWindow : Driver.get().getWindowHandles()){
             Driver.get().switchTo().window(subWindow);
         }
 
+    // Click to apply for this job button
         descriptionPage.appyPositionBtn.click();
 
     }
@@ -57,7 +55,10 @@ public class applyStepDefs {
     @When("the user enters the necessary information on the info page")
     public void the_user_enters_the_necessary_information_on_the_info_page() throws InterruptedException {
 
+    // Upload the resume from PC
         infoPage.resumeBtn.sendKeys("C:\\Users\\vdw-Kursat\\Desktop\\Kursat Dogan - Resume.pdf");
+
+    // Enter other fake information with the help of javaFaker class
         infoPage.inputName.sendKeys(faker.name().fullName());
         infoPage.inputEmail.sendKeys(faker.internet().emailAddress());
         infoPage.inputPhone.sendKeys(faker.phoneNumber().cellPhone());
@@ -69,17 +70,23 @@ public class applyStepDefs {
         infoPage.inputUrl.get(4).sendKeys(faker.internet().url());
         infoPage.inputAvabilityAndSlary.get(0).sendKeys("4 weeks");
         infoPage.inputAvabilityAndSlary.get(1).sendKeys("**** Euro/Year Gross");
+
+    // I put some explicit wait here to  click the radio buttons
         WebDriverWait wait = new WebDriverWait(Driver.get(), 10);
         wait.until(ExpectedConditions.elementToBeClickable(infoPage.placeRadioBtn));
         wait.until(ExpectedConditions.elementToBeClickable(infoPage.cypressRadioBtn));
         infoPage.placeRadioBtn.click();
         infoPage.cypressRadioBtn.click();
+
+    // I have put here some Thread to handle Capthcha manually
         Thread.sleep(30000);
         infoPage.submitBtn.click();
     }
 
     @Then("the user should able to navigate application page")
     public void the_user_should_able_to_navigate_application_page() {
+
+    // Verifying the test with Junit assertion
         String actualMsg = verifyPage.applicationMsg.getText();
         String expectedMsg = "Application submitted!";
         Assert.assertEquals(expectedMsg,actualMsg);
